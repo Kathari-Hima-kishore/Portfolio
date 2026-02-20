f68f21237fcf4be1d3650c92fc315ed7d8cd49cd
Hima Kishore <Kathari-Hima-kishore@users.noreply.github.com>
2026-02-20 21:43:27 +0530

    fix

--- src/components/ui/MobileWarning.tsx
+++ src/components/ui/MobileWarning.tsx
@@ -33,7 +33,7 @@ export const MobileWarning = ({ children }: { children: React.ReactNode }) => {
                         This Portfolio is not optimized for portrait screens and Mobile Devices.
                     </p>
                     <p className="text-white/50 text-sm">
-                        Please switch to <strong>Desktop mode</strong> or use a <strong>Laptop</strong>for the intended functionality.
+                        Please switch to <strong>Laptop </strong> or use a <strong>Laptop/Desktop </strong>for the intended functionality.
                     </p>
                 </div>
             </div>
